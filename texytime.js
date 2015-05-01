5d452e9547c19aa279afdb76672014f4b52010c0
dcr24 <dcr24@sussex.ac.uk>
2015-05-01 14:57:35 +0100

    removed comments

--- texytime.js
+++ texytime.js
@@ -1,6 +1,4 @@
 javascript: (function() {
-    // thanks to jlong for this handy tip
-    // https://gist.github.com/jlong/2428561
     var parser = document.createElement('a');
     parser.href = window.location.href;
     var refname = prompt('enter the name of your citiation', 'online:'+parser.hostname);
