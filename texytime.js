e198fdfe6acbb7b367987c432bfead6ed8916b88
dcr24 <dcr24@sussex.ac.uk>
2015-05-01 15:01:22 +0100

    testing alert text

--- texytime.js
+++ texytime.js
@@ -1,5 +1,7 @@
 javascript: (function() {
     var parser = document.createElement('a');
     parser.href = window.location.href;
-    var refname = prompt('enter the name of your citiation', 'online:'+parser.hostname);
+    var refname = prompt('Enter the name of your citiation', 'online:'+parser.hostname);
+    var refString = '@online{' + refname + ',\n}';
+    alert(refString);
 }());
