ba705883c1f01594c984d228629717956a030891
dcr24 <dcr24@sussex.ac.uk>
2015-05-01 14:56:14 +0100

    testing popup box for name entry

--- texytime.js
+++ texytime.js
@@ -1,3 +1,7 @@
 javascript: (function() {
-    alert('test');
+    // thanks to jlong for this handy tip
+    // https://gist.github.com/jlong/2428561
+    var parser = document.createElement('a');
+    parser.href = window.location.href;
+    var refname = prompt('enter the name of your citiation', 'online:'+parser.hostname);
 }());
