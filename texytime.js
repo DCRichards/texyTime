ca393c9257216177e71f2ecbad7145dff6cee72f
dcr24 <dcr24@sussex.ac.uk>
2015-05-01 15:28:33 +0100

    get full year not just year

--- texytime.js
+++ texytime.js
@@ -3,7 +3,7 @@ javascript: (function() {
     parser.href = window.location.href;
     var refname = prompt('Enter the name of your citiation', 'online:'+parser.hostname);
     var title = document.title;
-    var viewDate = new Date().getDate() + '-' + new Date().getMonth() + '-' + new Date().getYear();
+    var viewDate = new Date().getDate() + '-' + new Date().getMonth() + '-' + new Date().getFullYear();
     var refString = '@online{' + refname + ',\n'
             + '\tauthor = {},\n'
             + '\ttitle = {{' + title + '}},\n'
