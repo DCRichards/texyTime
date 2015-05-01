6d79c6023edcbf68481218a3efc77a0ad265fbf7
dcr24 <dcr24@sussex.ac.uk>
2015-05-01 15:09:32 +0100

    added test bibtex string

--- texytime.js
+++ texytime.js
@@ -2,6 +2,14 @@ javascript: (function() {
     var parser = document.createElement('a');
     parser.href = window.location.href;
     var refname = prompt('Enter the name of your citiation', 'online:'+parser.hostname);
-    var refString = '@online{' + refname + ',\n}';
+    var title = document.title;
+    var viewDate = new Date();
+    var refString = '@online{' + refname + ',\n'
+            + '\tauthor = {},\n'
+            + '\ttitle = {{' + title + '}},\n'
+            + '\tyear = ' + viewDate.getFullYear() + ',\n'
+            + '\turl = {' + parser.href + '},\n'
+            + '\turldate = {' + viewDate.toDateString() + '}\n'
+            + '}';
     alert(refString);
 }());
